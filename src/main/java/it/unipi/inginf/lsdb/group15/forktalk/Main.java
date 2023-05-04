ec82daf099e3532c640caede82e005d6ff7ac71a
Your Name <you@example.com>
2023-05-04 12:44:07 +0200

    Prova

--- src/main/java/it/unipi/inginf/lsdb/group15/forktalk/Main.java
+++ src/main/java/it/unipi/inginf/lsdb/group15/forktalk/Main.java
@@ -2,6 +2,8 @@ package it.unipi.inginf.lsdb.group15.forktalk;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("prov");
     }
 }
