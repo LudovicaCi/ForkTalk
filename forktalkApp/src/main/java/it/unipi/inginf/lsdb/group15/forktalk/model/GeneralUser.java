66212a61b5cabc23757ab83f0e02275d8396609e
alicepetrillo <a.petrillo4@studenti.unipi.it>
2023-06-09 21:28:11 +0200

    Alice first commit

--- forktalkApp/src/main/java/it/unipi/inginf/lsdb/group15/forktalk/model/GeneralUser.java
+++ forktalkApp/src/main/java/it/unipi/inginf/lsdb/group15/forktalk/model/GeneralUser.java
@@ -9,32 +9,6 @@ public abstract class GeneralUser {
     private String password;
     private String email;
 
-    // METODI GET
-    public String getUsername() {
-
-        return username;
-    }
-    public String getPassword() {
-
-        return password;
-    }
-    public String getEmail() {
-
-        return email;
-    }
-    // METODI SET
-    public void setUsername(String username) {
-
-        this.username = username;
-    }
-    public void setPassword(String password) {
-
-        this.password = password;
-    }
-    public void setEmail(String email) {
-
-        this.email = email;
-    }
     //TO STRING
     @Override
     public String toString() {
